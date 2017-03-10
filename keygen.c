05b1ba8c4b964a84f55c5418e0eef5f25e474fac
Morgan <morganmbrenner@gmail.com>
2017-03-10 17:23:19 -0500

    added initial keygen.c

--- keygen.c
+++ keygen.c
@@ -2,3 +2,31 @@
 **	keygen.c
 **
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+	int i, numChars;
+	char randomKey[numChars];
+	char alphabet[27] = {
+					'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+					'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+					'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '
+					};
+
+	srand(time(NULL));
+
+	numChars = atoi(argv[1]);
+
+	//print all args 
+	for (i = 0; i < argc; i++) {
+		printf("%s ", argv[i]);
+	}
+	printf("\n");
+
+	//1st arg 
+	printf("%s\n", argv[1]);
+}
