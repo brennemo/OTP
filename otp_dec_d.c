4b61cc2e18c36617786bea9aac03c2d72e430209
Morgan <brennemo@oregonstate.edu>
2017-03-17 21:43:33 -0400

    read loop for dec_d

--- otp_dec_d.c
+++ otp_dec_d.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	int i;
 	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
 	socklen_t sizeOfClientInfo;
-	char buffer[BUFFER_SIZE];
+	char buffer[BUFFER_SIZE], readBuffer[BUFFER_SIZE];
 	struct sockaddr_in serverAddress, clientAddress;
 	char temp, key, encrypt;
 	
@@ -84,8 +84,16 @@ int main(int argc, char *argv[])
 		else if (childPid == 0) {
 			
 			memset(buffer, '\0', BUFFER_SIZE);
-			charsRead = recv(establishedConnectionFD, buffer, BUFFER_SIZE - 1, 0); // Read the client's message from the socket
-			if (charsRead < 0) error("ERROR reading from socket");
+			while(strstr(buffer, "@@") == NULL) {
+				memset(readBuffer, '\0', BUFFER_SIZE);
+				charsRead = recv(establishedConnectionFD, readBuffer, BUFFER_SIZE - 1, 0); // Read the client's message from the socket
+				strcat(buffer,readBuffer);
+				if (charsRead < 0) error("ERROR reading from socket");
+			}
+			
+			//memset(buffer, '\0', BUFFER_SIZE);
+			//charsRead = recv(establishedConnectionFD, buffer, BUFFER_SIZE - 1, 0); // Read the client's message from the socket
+			//if (charsRead < 0) error("ERROR reading from socket");
 			//printf("SERVER: I received this from the client: \"%s\"\n", buffer);
 			
 			//printf("BUFFER IN SERVER: %s\n", buffer);
